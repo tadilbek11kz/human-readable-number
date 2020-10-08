64ed2301a43f490698716d62851a88b5547ae05c
Adilbek11kz <tadilbek11kz@gmail.com>
2020-10-09 01:20:40 +0600

    init: create human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(n) {
+    var string = n.toString(), units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
+    string = string.replace(/[, ]/g, "");
+    if (parseInt(string) === 0) {
+        return 'zero';
+    }
+
+    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    scales = ['', 'thousand'];
+
+    start = string.length;
+    chunks = [];
+    while (start > 0) {
+        end = start;
+        chunks.push(string.slice((start = Math.max(0, start - 3)), end));
+    }
+    chunksLen = chunks.length;
+    if (chunksLen > scales.length) {
+        return '';
+    }
+    words = [];
+    for (i = 0; i < chunksLen; i++) {
+
+        chunk = parseInt(chunks[i]);
+
+        if (chunk) {
+            ints = chunks[i].split('').reverse().map(parseFloat);
+            if (ints[1] === 1) {
+                ints[0] += 10;
+            }
+            if ((word = scales[i])) {
+                words.push(word);
+            }
+            if ((word = units[ints[0]])) {
+                words.push(word);
+            }
+            if ((word = tens[ints[1]])) {
+                words.push(word);
+            }
+            if ((word = units[ints[2]])) {
+                words.push(word + ' hundred');
+            }
+        }
+    }
+    return words.reverse().join(' ');
 }
